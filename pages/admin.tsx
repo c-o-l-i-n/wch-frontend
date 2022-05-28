1aa5fddf013a84b114d258d170a6f8f6f1983f89
Colin Williams <colin@colinaw.com>
2022-05-28 00:55:22 -0400

    🛠 Add /admin route to CMS

---
+++ pages/admin.tsx
@@ -0,0 +1,11 @@
+const Admin = () => {
+	return (
+		<iframe
+			src='https://williamscustomhomes.herokuapp.com/admin/content-manager'
+			frameBorder='0'
+			style={{ height: '100vh', width: '100vw' }}
+		></iframe>
+	)
+}
+
+export default Admin
