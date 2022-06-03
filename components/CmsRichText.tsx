23909bc1fae6445dd0a63bd47428a3b4106033be
Colin Williams <colin@colinaw.com>
2022-06-03 15:48:27 -0400

    🔗 Replace <a> with NextJS <Link> in CMS rich text

--- components/CmsRichText.tsx
+++ components/CmsRichText.tsx
@@ -1,8 +1,9 @@
 import SiteInformation from '../types/CmsSingleTypes/siteInformation'
 import { populateShortCodes } from '../utils/pipes'
-import parse from 'html-react-parser'
+import parse, { Element, domToReact } from 'html-react-parser'
 import { Box } from '@chakra-ui/react'
 import styles from '../styles/CmsRichText.module.scss'
+import Link from 'next/link'
 
 type Props = {
 	text: string
@@ -12,7 +13,18 @@ type Props = {
 const CmsRichText = ({ text, siteInfo }: Props) => {
 	return (
 		<Box className={styles.resetCSS}>
-			{parse(populateShortCodes(text, siteInfo))}
+			{parse(populateShortCodes(text, siteInfo), {
+				// replace <a> tags with Next <Link> components
+				replace: (domNode) => {
+					if (domNode instanceof Element && domNode.name === 'a') {
+						return (
+							<Link href={domNode.attribs.href} passHref>
+								<a>{domToReact(domNode.children)}</a>
+							</Link>
+						)
+					}
+				},
+			})}
 		</Box>
 	)
 }
