9dcf978e3c810088522b3de3e8fa93e494f341bb
Colin Williams <colin@colinaw.com>
2023-03-12 13:35:26 -0400

    ✍️ Refactor rich text component

--- src/components/CmsRichText.tsx
+++ src/components/CmsRichText.tsx
@@ -5,42 +5,40 @@ import { Box } from '@chakra-ui/react'
 import styles from '../styles/CmsRichText.module.scss'
 import Link from 'next/link'
 
+const isExternalLink = (url: string) => {
+	// if server side rendering (dev environment)
+	if (typeof window === 'undefined') {
+		return url.startsWith('http')
+	}
+
+	const tmp = document.createElement('a')
+	tmp.href = url
+	return tmp.host !== window.location.host
+}
+
 type Props = {
 	text: string
 	siteInfo: SiteInformation
 }
 
-const CmsRichText = ({ text, siteInfo }: Props) => {
-	const isExternalLink = (url: string) => {
-		// if server side rendering (dev environment)
-		if (typeof window === 'undefined') {
-			return url.startsWith('http')
-		}
-
-		const tmp = document.createElement('a')
-		tmp.href = url
-		return tmp.host !== window.location.host
-	}
-
-	return (
-		<Box className={styles.resetCSS}>
-			{parse(populateShortCodes(text, siteInfo), {
-				// replace <a> tags with Next <Link> components
-				replace: (domNode) => {
-					if (domNode instanceof Element && domNode.name === 'a') {
-						const url = domNode.attribs.href
-						return (
-							<Link href={url} passHref>
-								<a target={isExternalLink(url) ? '_blank' : undefined}>
-									{domToReact(domNode.children)}
-								</a>
-							</Link>
-						)
-					}
-				},
-			})}
-		</Box>
-	)
-}
+const CmsRichText = ({ text, siteInfo }: Props) => (
+	<Box className={styles.resetCSS}>
+		{parse(populateShortCodes(text, siteInfo), {
+			// replace <a> tags with Next <Link> components
+			replace: (domNode) => {
+				if (domNode instanceof Element && domNode.name === 'a') {
+					const url = domNode.attribs.href
+					return (
+						<Link href={url} passHref>
+							<a target={isExternalLink(url) ? '_blank' : undefined}>
+								{domToReact(domNode.children)}
+							</a>
+						</Link>
+					)
+				}
+			},
+		})}
+	</Box>
+)
 
 export default CmsRichText
